3bb9fb2ac97ae34cd9fda18d5acddb047ee6c435
Saulius Gudaitis <saulius.gudaitis@knf.stud.vu.lt>
2023-11-06 22:19:27 +0200

    Revert "Added backend code (bit of it)"
    This reverts commit cd50781db4b0316ce2f0616f97f4d8153e9518d1.

--- app/src/main/java/com/example/a3lab/MainActivity.java
+++ app/src/main/java/com/example/a3lab/MainActivity.java
@@ -3,75 +3,12 @@ package com.example.a3lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView calculatorScreen;
-    private String currentNumber;
-    private String operator;
-    private String function;
-    private double result = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        calculatorScreen = findViewById(R.id.calculatorScreen);
-        currentNumber = "";
-    }
-
-    public void onNumberClick(View view) {
-        Button button = (Button) view;
-        currentNumber += button.getText().toString();
-        calculatorScreen.setText(currentNumber);
-    }
-
-    public void onOperatorClick(View view) {
-        Button button = (Button) view;
-        operator = button.getText().toString();
-        result = Double.parseDouble(currentNumber);
-        currentNumber = "";
-    }
-
-    public void onFunctionClick(View view){
-        Button button = (Button) view;
-        function = button.getText().toString();
-    }
-
-    public void onEqualClick(View view) {
-        if (!currentNumber.equals("")) {
-            double secondNumber = Double.parseDouble(currentNumber);
-            switch (operator) {
-                case "+":
-                    result += secondNumber;
-                    break;
-                case "-":
-                    result -= secondNumber;
-                    break;
-                case "*":
-                    result *= secondNumber;
-                    break;
-                case "/":
-                    if (secondNumber != 0) {
-                        result /= secondNumber;
-                    } else {
-                        // handle division by zero
-                    }
-                    break;
-                // You can add more cases for other operations like %, square root, etc.
-            }
-            calculatorScreen.setText(String.valueOf(result));
-            currentNumber = String.valueOf(result);
-        }
-    }
-
-    public void onClearClick(View view) {
-        currentNumber = "";
-        calculatorScreen.setText("");
     }
 }
